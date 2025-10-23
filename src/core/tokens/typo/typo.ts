0a0f1682627ec0cb26f338e956a37b032e90ca57
justn-hyeok <justn.hyeok@gmail.com>
2025-10-23 15:08:36 +0900

    feat(#1): 디자인 토큰 추가

---
+++ src/core/tokens/typo/typo.ts
@@ -0,0 +1,58 @@
+const fontFamily = {
+  suit: ['SUIT Variable', 'sans-serif'],
+  icon: ['Material Symbols Rounded', 'monospace'],
+} as const;
+
+const fontWeight = {
+  heavy: 900,
+  extrabold: 800,
+  bold: 700,
+  semibold: 600,
+  medium: 500,
+  regular: 400,
+  light: 300,
+  extralight: 200,
+} as const;
+
+const fontSize = {
+  40: '40px',
+  32: '32px',
+  24: '24px',
+  20: '20px',
+  18: '18px',
+  16: '16px',
+  14: '14px',
+  13: '13px',
+  12: '12px',
+  11: '11px',
+} as const;
+
+const lineHeight = {
+  44: '44px',
+  34: '34px',
+  28: '28px',
+  26: '26px',
+  24: '24px',
+  22: '22px',
+  20: '20px',
+  18: '18px',
+  16: '16px',
+  14: '14px',
+} as const;
+
+const letterSpacing = {
+  5: '0.05em',
+  1: '0.01em',
+  0: '0em',
+  '-1': '-0.01em',
+  '-1.5': '-0.015em',
+  '-2': '-0.02em',
+} as const;
+
+export const typos = {
+  fontFamily,
+  fontSize,
+  fontWeight,
+  letterSpacing,
+  lineHeight
+} as const;
