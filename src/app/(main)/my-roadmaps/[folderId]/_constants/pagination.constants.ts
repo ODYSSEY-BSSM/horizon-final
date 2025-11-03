7caa0f3de10f6bbc83c0170a4e2803bddf5b1599
justn-hyeok <justn.hyeok@gmail.com>
2025-11-03 20:17:23 +0900

    chore(#22): 뭐 같아서 걍 [folderId]는 이슈 새로 파서 함 ㅅㄱ

--- src/app/(main)/my-roadmaps/[folderId]/_constants/pagination.constants.ts
+++
@@ -1,3 +0,0 @@
-export const LIST_VIEW_ITEMS_PER_PAGE = 10;
-export const CARD_VIEW_ITEMS_PER_PAGE = 12;
-export const DEFAULT_PAGE = 1;
--- src/app/(main)/my-roadmaps/[folderId]/page.tsx
+++
@@ -1,7 +0,0 @@
-import FolderContent from './content';
-
-export const dynamic = 'force-dynamic';
-
-export default function FolderPage() {
-  return <FolderContent />;
-}
